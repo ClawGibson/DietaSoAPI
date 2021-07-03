69a7f1315fdd43f3f0b5dfe29beca95c9ea73a8f
TulioA <tulio2799@gmail.com>
2021-07-02 21:49:13 -0500

    agregar atributos
    apellidos, celular, edad

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
-const usuariosSchema = new mongoose.Schema({
+const usuariosSchema = new mongoose.Schema(
+  {
     logros: { type: String, required: false },
     nombre: { type: String, required: true },
+    apellidoPaterno: { type: String, required: true },
+    apellidoMaterno: { type: String, required: true },
     foto: { type: String, required: false },
     email: { type: String, required: true },
     fechaDeNacimiento: { type: String, required: true },
     contrasena: { type: String, required: true },
     genero: { type: String, required: true },
+    celular: { type: String, required: false },
     peso: [Number],
+    edad: [Number],
     altura: { type: Number, required: true },
     actividadFisica: { type: String, required: true },
     historiaClinica: {
-        antecedentesPatologicos: [String],
-        antecedentesHeredoFamiliares: [String],
-        medicamentos: [String]
+      antecedentesPatologicos: [String],
+      antecedentesHeredoFamiliares: [String],
+      medicamentos: [String],
     },
     nivelSocioeconomico: {
-        ingresos: { type: String, required: false },
-        educacion: { type: String, required: false },
-        ocupacion: { type: String, required: false }
+      ingresos: { type: String, required: false },
+      educacion: { type: String, required: false },
+      ocupacion: { type: String, required: false },
     },
     comidaFavorita: [String],
     comidaNoFavorita: [String],
@@ -36,24 +41,26 @@ const usuariosSchema = new mongoose.Schema({
     colacion1Ayer: [String],
     comidaAyer: [String],
     colacion2Ayer: [String],
-    cenaAyer: [String]
-}, {
-    timestamps: true
+    cenaAyer: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+usuariosSchema.method("toJSON", function () {
+  const { __v, ...object } = this.toObject();
+  const { _id: id, ...result } = object;
+  return { ...result, id };
 });
 
-usuariosSchema.method('toJSON', function () {
-    const { __v, ...object } = this.toObject();
-    const { _id: id, ...result } = object;
-    return { ...result, id };
+usuariosSchema.virtual("id").get(function () {
+  return this._id.toHexString();
 });
 
-usuariosSchema.virtual('id').get(function () {
-    return this._id.toHexString();
+usuariosSchema.set("toJSON", {
+  virtuals: true,
 });
 
-usuariosSchema.set('toJSON', {
-    virtuals: true
-});
-
-exports.Usuarios = mongoose.model('Usuarios', usuariosSchema);
+exports.Usuarios = mongoose.model("Usuarios", usuariosSchema);
 exports.usuariosSchema = usuariosSchema;
