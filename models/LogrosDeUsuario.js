7ddb3ab92b2838ef89fe04ba4defecfd0e47ef6b
TulioA <tulio2799@gmail.com>
2021-07-08 22:23:00 -0500

    datos
    Faltan los últimos de alimentación

--- models/LogrosDeUsuario.js
+++ models/LogrosDeUsuario.js
@@ -1,16 +1,17 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const logrosDeUsuarioSchema = new Schema({
-  logro: { type: Schema.Types.ObjectId, ref: 'Logros', required: false },
-  usuario: { type: Schema.Types.ObjectId, ref: 'Usuarios', required: true },
+  logro: { type: Schema.Types.ObjectId, ref: "Logros", required: false },
+  usuario: { type: Schema.Types.ObjectId, ref: "Usuarios", required: true },
 });
+//Siempre poner la referencia en plural- plural del modelo
 
-logrosDeUsuarioSchema.virtual('id').get(() => {
+logrosDeUsuarioSchema.virtual("id").get(() => {
   return this._id.toHexString();
 });
 
-logrosDeUsuarioSchema.set('toJSON', {
+logrosDeUsuarioSchema.set("toJSON", {
   virtuals: true,
 });
 
-module.exports = model('LogrosDeUsuario', logrosDeUsuarioSchema);
+module.exports = model("LogrosDeUsuario", logrosDeUsuarioSchema);
