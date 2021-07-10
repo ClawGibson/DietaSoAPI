2a926aeeb5f6f7951c0bc43cfb3c075cb03396ff
TulioA <tulio2799@gmail.com>
2021-07-10 11:43:58 -0500

    Update LogrosDeUsuario.js

--- models/LogrosDeUsuario.js
+++ models/LogrosDeUsuario.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const logrosDeUsuarioSchema = new Schema({
-  logro: { type: Schema.Types.ObjectId, ref: "Logros", required: false },
+  logro: [{ type: Schema.Types.ObjectId, ref: "Logros", required: false }],
   usuario: { type: Schema.Types.ObjectId, ref: "Usuarios", required: true },
 });
 //Siempre poner la referencia en plural- plural del modelo
