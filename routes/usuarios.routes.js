8fc717c896c6fa4c956d429b21831467eda48ad9
Gamaliel Bernal <gamalielbj@gmail.com>
2021-07-24 15:37:22 -0500

    Fixed the [ERR_HTTP_HEADERS_SENT]

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,13 +1,13 @@
-const { Usuarios } = require("../models/Usuarios");
-const express = require("express");
+const { Usuarios } = require('../models/Usuarios');
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   let listaUsuarios;
   try {
-    listaUsuarios = await Usuarios.find().select("-contrasena");
+    listaUsuarios = await Usuarios.find().select('-contrasena');
   } catch (error) {
     console.log(error);
   }
@@ -17,17 +17,17 @@ router.get("/", async (req, res) => {
   res.send(listaUsuarios);
 });
 
-router.get("/:id", async (req, res) => {
-  const usuario = await Usuarios.findById(req.params.id).select("-contrasena");
+router.get('/:id', async (req, res) => {
+  const usuario = await Usuarios.findById(req.params.id).select('-contrasena');
 
   if (!usuario)
     return res
       .status(500)
-      .json({ success: false, message: "Usuario no encontrado" });
+      .json({ success: false, message: 'Usuario no encontrado' });
   res.send(usuario);
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   let crearUsuario = new Usuarios({
     logros: req.body.logros,
 
@@ -41,17 +41,17 @@ router.post("/", async (req, res) => {
   crearUsuario = await crearUsuario.save();
 
   if (!crearUsuario)
-    return res.status(400).send("No se pudo crear el usuario :c");
+    return res.status(400).send('No se pudo crear el usuario :c');
 
   res.send(crearUsuario);
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const usuario = await Usuarios.findOne({ email: req.body.email });
   const SECRET = process.env.SECRET;
 
   if (!usuario) {
-    return res.status(400).send("Usuario no registrado :c");
+    return res.status(400).send('Usuario no registrado :c');
   }
 
   if (usuario && bcrypt.compareSync(req.body.contrasena, usuario.contrasena)) {
@@ -61,48 +61,46 @@ router.post("/login", async (req, res) => {
         isAdmin: usuario.esAdmin,
       },
       SECRET,
-      { expiresIn: "1y" }
+      { expiresIn: '1y' }
     );
     res.status(200).send({ usuario: usuario.email, token: token });
   } else {
-    res.status(400).send("Contraseña incorrecta");
+    res.status(400).send('Contraseña incorrecta');
   }
 });
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const usuario = await Usuarios.findOne({ email: req.body.email });
   try {
     if (usuario)
       return res
         .status(500)
-        .json({ success: false, message: "Usuario ya creado" });
-    res.send(usuario);
+        .json({ success: false, message: 'Usuario ya creado' });
   } catch (err) {
-    console.log("Ocurrió un error al buscar el usuario - ", err);
+    console.log('Ocurrió un error al buscar el usuario - ', err);
   }
-
-  let registrarUsuario = new Usuarios({
-    logros: req.body.logros,
-
-    email: req.body.email,
-
-    contrasena: bcrypt.hashSync(req.body.contrasena, 10),
-
-    tipoDeUsuario: req.body.tipoDeUsuario,
-  });
-
   try {
-    const datosGuardados = await registrarUsuario.save();
+    let registrarUsuario = new Usuarios({
+      logros: req.body.logros,
 
-    if (!datosGuardados)
-      return res.status(400).send("No se pudo agregar al usuario");
-    res.send(datosGuardados);
+      email: req.body.email,
+
+      contrasena: bcrypt.hashSync(req.body.contrasena, 10),
+
+      tipoDeUsuario: req.body.tipoDeUsuario,
+    });
+
+    registrarUsuario = await registrarUsuario.save();
+
+    if (!registrarUsuario)
+      return res.status(400).send('No se pudo agregar al usuario');
+    res.send(registrarUsuario);
   } catch (err) {
-    console.log("Ocurrió un error al guardar usuario - ", err);
+    console.log('Ocurrió un error al guardar usuario - ', err);
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put('/:id', async (req, res) => {
   let editarUsuario = await Usuarios.findOneAndUpdate(req.params.id, {
     logros: req.body.logros,
 
@@ -116,7 +114,7 @@ router.put("/:id", async (req, res) => {
   editarUsuario = await editarUsuario.save();
 
   if (!editarUsuario)
-    return res.status(400).send("No se pudo editar el usuario :c");
+    return res.status(400).send('No se pudo editar el usuario :c');
 
   res.send(editarUsuario);
 });
