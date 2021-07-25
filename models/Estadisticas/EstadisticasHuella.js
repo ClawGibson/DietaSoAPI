6d528a09465137146b7cba4656f6d61e201692bd
Gamaliel Bernal <gamalielbj@gmail.com>
2021-07-24 20:00:48 -0500

    Added some models
    - Estadísticas.
    - EconomíaFichas.
    - IMC.
    - Huella.
    
    Modified the RegistroDietetico model also

---
+++ models/Estadisticas/EstadisticasHuella.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const estadisticasHuellaSchema = new Schema(
+  {
+    usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' },
+    huellaHidricaTotal: { type: Number, default: 0 },
+    huellaCarbonoTotal: { type: Number, default: 0 },
+    puntajeEcologicoSostenible: { type: Number, default: 0 },
+    puntosEconomiaDeFichas: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+estadisticasHuellaSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+estadisticasHuellaSchema.set('toJSON', {
+  virtuals: true,
+});
+
+module.exports = model('EstadisticasHuella', estadisticasHuellaSchema);
---
+++ models/Estadisticas/EstadisticasIMC.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const estadisticasIMCSchema = new Schema(
+  {
+    usuario: { type: Schema.Types.ObjectId, ref: 'Usuarios', required: true },
+    peso: { type: Number, default: 0 },
+    IMC: { type: Number, default: 0 },
+    porcentajeGrasaCorporal: { type: Number, default: 0 },
+    porcentajeMasaMuscular: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+estadisticasIMCSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+estadisticasIMCSchema.set('toJSON', {
+  virtuals: true,
+});
+
+module.exports = model('EstadisticasIMC', estadisticasIMCSchema);
