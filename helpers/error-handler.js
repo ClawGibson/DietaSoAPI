b7bb04a5581303b65d4279bc94e92178cedb76d8
Gama <gamalielbj@gmail.com>
2021-09-11 18:21:24 -0500

    Modified the jwt auth

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,10 +1,9 @@
 function errorHandler(err, req, res, next) {
-
     if (err.name === 'UnauthorizedError') {
-        return res.status(401).json({ message: 'Acceso no autorizado' });
+        return res.status(401).send({ message: 'Acceso no autorizado' });
     }
     if (err.name === 'ValidationError') {
-        return res.status(401).json({ message: err });
+        return res.status(401).send({ message: err });
     }
 
     return res.status(500).json(err);
