4805b7043634bb61f8c51734272b142ccd0eca3c
Gama <gamalielbj@gmail.com>
2021-11-04 18:18:15 -0600

    First try to get chatId

--- routes/Chat/chat.routes.js
+++ routes/Chat/chat.routes.js
@@ -1,11 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { createNewChat, getAllChats, deleteChat } = require("./controller");
+const {
+    createNewChat,
+    getAllChats,
+    deleteChat,
+    getChatId,
+} = require('./controller');
 
-router.post("/", createNewChat);
-router.get("/", getAllChats);
-router.delete("/:id", deleteChat);
-
+router.post('/', createNewChat);
+router.get('/', getAllChats);
+router.get('/:users', getChatId);
+router.delete('/:id', deleteChat);
 
 module.exports = router;
--- routes/Chat/controller.js
+++ routes/Chat/controller.js
@@ -1,31 +1,52 @@
-const { response } = require("express");
-const Chat = require("../../models/Chat/Chat");
+const { response } = require('express');
+const Chat = require('../../models/Chat/Chat');
 
 const createNewChat = async (req, res = response) => {
     try {
         let newChat = new Chat({
             users: req.body.users,
-        })
+        });
         newChat = await newChat.save();
         if (!newChat) {
-            return res.status(401).json({ msg: "Son necesarios los usuarios" })
+            return res.status(401).json({ msg: 'Son necesarios los usuarios' });
         }
-        res.status(200).json("Creado correctamente");
+        res.status(200).json('Creado correctamente');
     } catch (error) {
         console.log(error);
-        return res.status(500).send({ error })
+        return res.status(500).send({ error });
     }
 };
-
 
 const getAllChats = async (req, res = response) => {
     try {
         const allChats = await Chat.find();
         res.status(200).json({
-            chats: allChats
+            chats: allChats,
         });
     } catch (error) {
-        res.status(500).json({ error })
+        res.status(500).json({ error });
+    }
+};
+
+const getChatId = async (req, res = response) => {
+    try {
+        const { adminId, userId } = req.params.users;
+        const chat = await Chat.find({
+            users: {
+                $all: [adminId, userId],
+            },
+        });
+
+        if (!chat) {
+            res.status(400).json({
+                msg: 'No se ha encontrado el chat seleccionado',
+            });
+        }
+        res.status(200).json({
+            chat,
+        });
+    } catch (error) {
+        res.status(400).json({ error });
     }
 };
 
@@ -34,20 +55,20 @@ const deleteChat = async (req, res = response) => {
         const _id = req.params.id;
         const chat = await Chat.findOneAndDelete({ _id });
         if (!chat) {
-            res.status(400).json({ msg: "No se ha encontrado el chat seleccionado" })
+            res.status(400).json({
+                msg: 'No se ha encontrado el chat seleccionado',
+            });
         }
         res.status(200).json({
-            msg: "Chat eliminado"
-        })
+            msg: 'Chat eliminado',
+        });
     } catch (error) {
-        res.status(400).json({ error })
-
+        res.status(400).json({ error });
     }
-
-
 };
 module.exports = {
     createNewChat,
     getAllChats,
+    getChatId,
     deleteChat,
-}
+};
