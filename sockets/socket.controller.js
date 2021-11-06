7d2056d20924a0cafc33d7af1a58f5c5bf075ba5
Gama <gamalielbj@gmail.com>
2021-11-05 19:41:35 -0600

    Update socket.controller.js

--- sockets/socket.controller.js
+++ sockets/socket.controller.js
@@ -4,17 +4,18 @@ const socketController = (socket) => {
     });
 
     socket.on('enviar-mensaje', (payload, callback) => {
-        console.log(payload);
+        console.log(`[enviar-mensaje]: ${payload}`);
         //socket.broadcast.emit('enviar-mensaje', payload); // Enviar el mensaje a todos los sockets conectados con broadcast.
         callback();
     });
 
     socket.on('mensaje-recibido', (payload, callback) => {
+        console.log(`[mensaje-recibido]: ${payload}`);
         socket.emit('enviar-mensaje', callback);
     });
 
     socket.on('crear-chat', (payload, callback) => {
-        console.log('payload:', payload);
+        console.log(`[crear-chat]: ${payload}`);
 
         try {
             callback();
