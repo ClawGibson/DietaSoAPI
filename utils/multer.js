e44aa2659bbf1be9c9536f84cdbde926b7d036ef
Gama <gamalielbj@gmail.com>
2021-11-17 18:55:59 -0600

    Update multer.js

--- utils/multer.js
+++ utils/multer.js
@@ -3,12 +3,12 @@ const multer = require('multer');
 module.exports = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
-        
-        if(!file.mimetype.match(/png||jpeg||jpg||gif$i/)){
-            cb( new Error('File does not support'), false);
+        if (!file.mimetype.match(/png||jpeg||jpg||gif$i/)) {
+            cb(new Error('File does not support'), false);
             return;
         }
 
         cb(null, true);
-    }
+    },
+    limits: { fieldSize: 25 * 1024 * 1024 },
 });
