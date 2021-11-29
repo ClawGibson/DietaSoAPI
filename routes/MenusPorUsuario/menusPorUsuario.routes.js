b7cd58dee921cfcefa16ba914b77ac80d59e8820
Gama <gamalielbj@gmail.com>
2021-11-28 18:21:19 -0600

    Update menusPorUsuario.routes.js

--- routes/MenusPorUsuario/menusPorUsuario.routes.js
+++ routes/MenusPorUsuario/menusPorUsuario.routes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 router.get('/:userId', async (req, res) => {
     try {
-        const registro = await MenusPorUsuario.findOne({
+        const registro = await MenusPorUsuario.find({
             usuario: req.params.userId,
         }).populate('menu usuario');
 
