931fcf26cf907409cfa513659ffea336844783dd
Gama <gamalielbj@gmail.com>
2022-03-22 17:36:48 -0600

    Update socket.controller.js

--- sockets/socket.controller.js
+++ sockets/socket.controller.js
@@ -4,14 +4,12 @@ const Message = require('../models/Message/message');
 const InformacionUsuarios = require('../models/InformacionUsuarios');
 
 const socketController = (socket) => {
-
-    socket.on("get-chat", async ({id, isAdmin, patientId}) =>{
-        
+    socket.on('get-chat', async ({ id, isAdmin, patientId }) => {
         let userPatientId;
 
-        if(patientId){
+        if (patientId) {
             userPatientId = await InformacionUsuarios.findOne({
-                _id: patientId
+                _id: patientId,
             });
         }
 
@@ -19,71 +17,64 @@ const socketController = (socket) => {
 
         //Get User chat
         let chat = await Chat.findOne({
-            users: isAdmin? userPatientId.usuario:id 
+            users: isAdmin ? userPatientId.usuario : id,
         });
 
-
         //Creamos un chat con el administrador
-        if(!chat && !isAdmin){
+        if (!chat && !isAdmin) {
             //buscamos al administrador
-            console.log("EL paciente creo el chat")
-            const admin = await Usuario.findOne({ esAdmin: true});
+            console.log('EL paciente creo el chat');
+            const admin = await Usuario.findOne({ esAdmin: true });
             users.push(id, admin._id);
             //enviamos el id del chat.
-        } else if(!chat && isAdmin){
+        } else if (!chat && isAdmin) {
             //Creamos un chat con el usuario seleccionad
-            console.log("EL administrador creo el chat")
+            console.log('EL administrador creo el chat');
             users.push(id, userPatientId.usuario);
         }
-        
+
         let messages = [];
 
-        if(!chat){
+        if (!chat) {
             chat = new Chat({ users });
             await chat.save();
-        }else{
+        } else {
             messages = await Message.find({
-                chat: chat._id
+                chat: chat.chat,
             })
-            .sort({date: "desc" })
-            .limit(10)
-            .skip(1)
+                .sort({ date: 'desc' })
+                .limit(15)
+                .skip(1);
         }
 
         socket.join(String(chat._id));
-        socket.emit("getMessages", {
+        socket.emit('getMessages', {
             chatId: chat._id,
-            messages
+            messages,
         });
-
     });
 
-
-    socket.on("sendMessage", async(data) => {
-
+    socket.on('sendMessage', async (data) => {
         const msg = new Message({
             chat: data.chat,
             message: data.message.text,
             user: data.user,
-            date: new Date()
+            date: new Date(),
         });
 
         await msg.save();
 
         data.message.user._id = 2;
 
-        socket.to(String(data.chat)).emit("rm", {
-          message: data.message
+        socket.to(String(data.chat)).emit('rm', {
+            message: data.message,
         });
-
-
     });
 
-    socket.on("disconnect", () => {
-        console.log("disconnect")
+    socket.on('disconnect', () => {
+        console.log('disconnect');
         socket.rooms.size === 0;
     });
-
 };
 
 module.exports = { socketController };
