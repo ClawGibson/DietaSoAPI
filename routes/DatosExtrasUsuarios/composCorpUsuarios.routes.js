8297c710962de8d3e16a264a52bc9363256c887d
Gama <gamalielbj@gmail.com>
2022-03-25 18:52:29 -0600

    Quick fix

--- routes/DatosExtrasUsuarios/composCorpUsuarios.routes.js
+++ routes/DatosExtrasUsuarios/composCorpUsuarios.routes.js
@@ -42,57 +42,28 @@ router.get('/individual', async (req, res) => {
 
 router.post('/individual', async (req, res) => {
     try {
-        const usuarioCreado = await Usuarios.findOne({
+        let dComposCorp = new ComposCorpUsuarios({
             usuario: req.query.usuario,
+            porcentGrasa: req.body.porcentGrasa,
+            porcentMasa: req.body.porcentMasa,
+            porcentAgua: req.body.porcentAgua,
+            densidadOsea: req.body.densidadOsea,
+            grasaVisceral: req.body.grasaVisceral,
+            tasaMetabolica: req.body.tasaMetabolica,
+            edadMetabolica: req.body.edadMetabolica,
         });
-        if (usuarioCreado) {
-            const infoUsuario = await ComposCorpUsuarios.findOne({
-                usuario: req.query.usuario,
-            });
-            try {
-                if (infoUsuario)
-                    return res.status(500).json({
-                        success: false,
-                        message:
-                            'Datos de composición corporal de Usuario ya registrados',
-                    });
-            } catch (err) {
-                return res.status(500).json({
-                    success: false,
-                    message:
-                        'Ocurrió un error al buscar los datos de composición corporal del usuario',
-                });
-            }
-        } else console.log('El usuario no existe');
-    } catch (err) {
-        return res.status(500).json({
-            success: false,
-            message: 'Ocurrió un error al buscar al usuario',
-        });
-    }
 
-    let dComposCorp = new ComposCorpUsuarios({
-        usuario: req.query.usuario,
-        porcentGrasa: req.body.porcentGrasa,
-        porcentMasa: req.body.porcentMasa,
-        porcentAgua: req.body.porcentAgua,
-        densidadOsea: req.body.densidadOsea,
-        grasaVisceral: req.body.grasaVisceral,
-        tasaMetabolica: req.body.tasaMetabolica,
-        edadMetabolica: req.body.edadMetabolica,
-    });
-
-    try {
         dComposCorp = await dComposCorp.save();
 
         if (!dComposCorp)
             return res
                 .status(400)
                 .send('No se pudieron agregar datos de composición corporal');
-        res.send(dComposCorp);
+        res.status(200).send(dComposCorp);
     } catch (err) {
         return res.status(500).json({
             success: false,
+            error: err,
             message:
                 'Ocurrió un error al guardar los datos de composición corporal',
         });
@@ -101,50 +72,37 @@ router.post('/individual', async (req, res) => {
 
 router.patch('/individual', async (req, res) => {
     try {
-        const existeUsuario = await buscarUsuario(req.query.usuario);
-        let editarInformacionS;
-        if (!existeUsuario)
-            return res
-                .status(500)
-                .json({ success: false, message: 'El usuario no existe.' });
+        editarInformacionS = await ComposCorpUsuarios.findOneAndUpdate(
+            { usuario: req.query.usuario },
+            {
+                $push: {
+                    porcentGrasa: req.body.porcentGrasa,
+                    porcentMasa: req.body.porcentMasa,
+                    porcentAgua: req.body.porcentAgua,
+                    densidadOsea: req.body.densidadOsea,
+                    grasaVisceral: req.body.grasaVisceral,
+                    tasaMetabolica: req.body.tasaMetabolica,
+                    edadMetabolica: req.body.edadMetabolica,
+                },
+            }
+        );
 
-        try {
-            editarInformacionS = await ComposCorpUsuarios.findOneAndUpdate(
-                { usuario: existeUsuario.usuario },
-                {
-                    $push: {
-                        porcentGrasa: req.body.porcentGrasa,
-                        porcentMasa: req.body.porcentMasa,
-                        porcentAgua: req.body.porcentAgua,
-                        densidadOsea: req.body.densidadOsea,
-                        grasaVisceral: req.body.grasaVisceral,
-                        tasaMetabolica: req.body.tasaMetabolica,
-                        edadMetabolica: req.body.edadMetabolica,
-                    },
-                }
+        editarInformacionS = editarInformacionS
+            .save()
+            .then((response) => res.status(200).json({ message: 'ok' }))
+            .catch((err) =>
+                res.status(500).json({
+                    success: false,
+                    message: 'No se pudo guardar - ',
+                    err,
+                })
             );
-
-            editarInformacionS = editarInformacionS
-                .save()
-                .then((response) => res.status(200).json({ message: 'ok' }))
-                .catch((err) =>
-                    res.status(500).json({
-                        success: false,
-                        message: 'No se pudo guardar - ',
-                        err,
-                    })
-                );
-        } catch (err) {
-            res.status(500).json({
-                success: false,
-                message:
-                    ' Ocurrió un error al actualizar los datos de composición corporal- ',
-            });
-        }
     } catch (err) {
         res.status(500).json({
             success: false,
-            message: ' Ocurrió un error al buscar el usuario- ',
+            error: err,
+            message:
+                ' Ocurrió un error al actualizar los datos de composición corporal- ',
         });
     }
 });
