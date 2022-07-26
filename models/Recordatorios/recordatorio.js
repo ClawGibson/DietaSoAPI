9610042ec5f7a1ac1331b101e8438fef2f8231c9
Gama <gamalielbj@gmail.com>
2022-07-25 20:29:44 -0500

    Update recordatorio.js

--- models/Recordatorios/recordatorio.js
+++ models/Recordatorios/recordatorio.js
@@ -3,82 +3,18 @@ const { Schema, model } = require('mongoose');
 const recordatorioModel = new Schema(
     {
         usuarios: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'InformacionUsuarios',
-                required: true,
-            },
+            { type: Schema.Types.ObjectId, ref: 'InformacionUsuarios', required: true },
         ],
         usuariosConfirmados: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'InformacionUsuarios',
-                required: true,
-            },
+            { type: Schema.Types.ObjectId, ref: 'InformacionUsuarios', required: true },
         ],
-        metas: {
-            type: Schema.Types.ObjectId,
-            ref: 'Metas',
-            required: false,
-        },
-        titulo: {
-            type: String,
-            required: true,
-        },
-        mensaje: {
-            type: String,
-            required: true,
-        },
-        categoria: {
-            type: String,
-            required: false,
-        },
-        fecha: [
-            {
-                type: Date,
-            },
-        ],
-        hora: [
-            {
-                type: Date,
-            },
-        ],
-        // dias: [{
-        //     day: {
-        //         type: String,
-        //         required: true,
-        //     },
-        //     dias: [
-        //         {
-        //             day: {
-        //                 type: String,
-        //                 required: true,
-        //             },
-        //             activo: {
-        //                 type: Boolean,
-        //                 default: false,
-        //             },
-        //         },
-        //     ],
-        //     // horario: {
-        //     //     type: String,
-        //     //     required: false, // Para prueba en false
-        //     // },
-
-        //     global: {
-        //         type: Boolean,
-        //         default: false,
-        //     }
-        // }],
-        // horario: {
-        //     type: String,
-        //     required: false, // Para prueba en false
-        // },
-
-        global: {
-            type: Boolean,
-            default: false,
-        },
+        metas: { type: Schema.Types.ObjectId, ref: 'Metas', required: false },
+        titulo: { type: String, required: true, unique: false },
+        mensaje: { type: String, required: true },
+        categoria: { type: String, required: false },
+        fecha: [{ type: Date }],
+        hora: [{ type: Date }],
+        global: { type: Boolean, default: false },
     },
     { timestamp: true }
 );
